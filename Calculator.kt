f0a6d0bac3cfc9b5c8efc1320c5153e0f0c3ae8f
Yejin-Byun <128487386+Yejin-Byun@users.noreply.github.com>
2024-03-14 11:26:44 +0900

    Delete Calculator.kt

--- Calculator.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testest
-
-open class Calculator(num1: Int, num2: Int): AbstractOperation(num1, num2) {
-    override fun operation() {
-        println("연산을 수행합니다 . . .")
-    }
-}
