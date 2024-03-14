3f63be801c30200c01c8cb6400fc6ad2b1494749
Yejin-Byun <128487386+Yejin-Byun@users.noreply.github.com>
2024-03-14 11:27:10 +0900

    Delete MultiplyOperation.kt

--- MultiplyOperation.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testest
-
-class MultiplyOperation(num1: Int, num2: Int): AbstractOperation(num1, num2) {
-    override fun operation() {
-        println("$num1 * $num2 = ${num1*num2}")
-        println()
-    }
-}
